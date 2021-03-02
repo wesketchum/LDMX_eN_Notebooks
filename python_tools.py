5fde55a5106d97320822fbb62c995abcdf5681d7
Wesley Ketchum <wesketchum@gmail.com>
2021-03-02 14:40:48 -0600

    initial commit of some basic tools

---
+++ python_tools.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+#imports
+import uproot
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from math import *
+import scipy as sp
+import scipy.stats
+import scipy.optimize
+import glob
+import time
+import particle
+
+try:
+    import numba
+    from numba import jit, int32, float32
+    import numba_scipy
+except:
+    print("Cannot find numba. Don't use numba here!")
+    
+#matplotlib options
+plt.rc('font', family='serif', size=12)
+pltops_hist = { "histtype":'step', "linewidth":1.5}
+plt.rc('image', cmap='seismic')
+plt.rcParams['figure.figsize'] = (10.0, 8.0)
+
+from cycler import cycler
+plt.rcParams['axes.prop_cycle'] = cycler(color=['b','g','r','c','m','y','k','indigo','orange','#137e6d'])
+
+def make_profile_bins(df,lowbin,hibin,nbins,xarg,yarg,option='sem',func='mean'):
+    xbins = np.linspace(lowbin,hibin,nbins+1)
+    diff = (xbins[1]-xbins[0])*0.00001
+    xbins[-1] = xbins[-1]+diff
+    result = (df[[xarg,yarg]].groupby(np.digitize(df[xarg],bins=xbins,right=False)))[yarg].agg([func,option])
+    result = result.reindex(range(1,len(xbins),1))
+    xbins[-1] = xbins[-1]-diff
+    result["x"] = 0.5*(xbins[:-1]+xbins[1:])
+    result["xerr"] = 0.5*(xbins[1]-xbins[0])
+    result.rename(columns={'mean': 'y', option: 'yerr'}, inplace=True)
+    return result
+
+def make_hists(df,hists,bins,axis,histtype='step',stacked=True):
+    n,cbins,patches = axis.hist([df.query(hd['cut'])[hd['var']] for hd in hists],
+                                weights=[df.query(hd['cut'])[hd['weight']] for hd in hists],
+                                label=["%s: %.1f"%(hd['label'],df.query(hd['cut'])[hd['weight']].sum()) for hd in hists],
+                                bins=bins,histtype=histtype,stacked=stacked)
+    for i in range(0,len(patches)):
+        hd = hists[i]
+        for p in patches[i]:
+            if 'color' in hd: p.set_color(hd['color'])
+            if 'facecolor' in hd: p.set_facecolor(hd['facecolor'])
+            if 'edgecolor' in hd: p.set_edgecolor(hd['edgecolor'])
+            if 'fill' in hd: p.set_fill(hd['fill'])
+            if 'hatch' in hd: p.set_hatch(hd['hatch'])
+            if 'linewidth' in hd: p.set_linewidth(hd['linewidth'])
+
+def tmp_plot(path="/Users/wketchum/Plots",ftype="pdf"):
+    return "%s/temp_%d.%s"%(path,np.round(time.time()),ftype)
+                
+print("python tools loaded.")
+                
+               
